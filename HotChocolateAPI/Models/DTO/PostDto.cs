e0dbe4aab6d558054ceffcc6bfe92f03d7f93452
denyo1337 <143619@student.uwm.edu.pl>
2021-05-26 19:33:38 +0200

    getAllPosts returns id for each post

--- HotChocolateAPI/Models/DTO/PostDto.cs
+++ HotChocolateAPI/Models/DTO/PostDto.cs
@@ -9,6 +9,7 @@ namespace HotChocolateAPI.Models.DTO
 {
     public class PostDto
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
